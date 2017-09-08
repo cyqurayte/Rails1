2727384cd8fe25746bc5d9edbc57d6418728046c
Cherian Grundmann <cherian.grundmann@gmail.com>
2017-09-08 21:33:35 +0200

    ex 4.5 result2

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
 
   get 'static_pages/index'
-  root 'static_pages#landing_page'
+
+  get 'static_pages/landing_page'
+
+  root 'static_pages#frontpage'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
